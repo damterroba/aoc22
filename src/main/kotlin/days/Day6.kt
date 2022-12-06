e4b5e82b3c88573454467b6d243b7be8c9c414d5
Tieu <mathieu.terroba@gmail.com>
2022-12-06 08:36:57 +0100

    Day6

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -1,11 +1,15 @@
 package days
 
-class Day6: Day() {
-    override fun part1(): Any {
-        TODO("Not yet implemented")
-    }
+class Day6 : Day() {
+    override fun part1(): Int = solve(4)
 
-    override fun part2(): Any {
-        TODO("Not yet implemented")
-    }
+    override fun part2(): Int = solve(14)
+    fun solve(n: Int) = input.windowed(n) { it.toSet() }.mapIndexed { index, chars -> index to (chars.size == n) }
+        .first { it.second == true }.first + n
+}
+
+fun main() {
+    val d = Day6()
+    println(d.part1())
+    println(d.part2())
 }
--- src/test/kotlin/days/Day6Tests.kt
+++ src/test/kotlin/days/Day6Tests.kt
@@ -1,5 +1,12 @@
 package days
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class Day6Tests {
     val d = Day6()
+    @Test
+    fun test(){
+        assertEquals(5, d.part1())
+    }
 }
