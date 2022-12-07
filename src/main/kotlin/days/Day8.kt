4ff9d0be4a40441ed22f590171eeaa196f90f675
Tieu <mathieu.terroba@gmail.com>
2022-12-07 09:40:28 +0100

    Prep day 8

---
+++ src/main/kotlin/days/Day8.kt
@@ -0,0 +1,17 @@
+package days
+
+class Day8: Day() {
+    override fun part1(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Any {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main(){
+    val d = Day8()
+    println(d.part1())
+    println(d.part2())
+}
---
+++ src/test/kotlin/days/Day8Tests.kt
@@ -0,0 +1,13 @@
+package days
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day8Tests {
+    val d= Day8()
+    
+    @Test
+    fun t(){
+
+    }
+}
