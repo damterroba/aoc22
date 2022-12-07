10132183d597f6d2681672378cb5bc2fac4dc3a4
Tieu <mathieu.terroba@gmail.com>
2022-12-07 09:35:30 +0100

    Add day7

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -1,11 +1,52 @@
 package days
 
-class Day7: Day() {
+
+fun <T> MutableList<T>.reset(default: T) {
+    this.clear()
+    this.add(default)
+}
+
+class Day7 : Day() {
     override fun part1(): Any {
-        TODO("Not yet implemented")
+        val disk = disk()
+        return disk.filter { it.value <= 100000 }.values.sum()
     }
 
     override fun part2(): Any {
-        TODO("Not yet implemented")
+        val diskSpaceTotal = 70000000
+        val minSize = 30000000
+        val disk = disk()
+        val freeSpace =  diskSpaceTotal-disk.maxOf {it.value }
+        return disk.filter { d -> d.value >= minSize-freeSpace}.minBy { it.value }.value
     }
+    
+    fun disk(): MutableMap<String, Int> {
+        val sizes = mutableMapOf<String, Int>()
+        val path = mutableListOf<String>()
+        input.split("\n").filterNot { it.startsWith("dir") }.map { "$it @".split(" ") }
+            .forEach { (dolOrInt, cmOrName, cdPath) ->
+                when (dolOrInt) {
+                    "$" -> when (cmOrName) {
+                        "cd" -> when (cdPath) {
+                            "/" -> path.reset("root")
+                            ".." -> path.removeLast()
+                            else -> path.add(cdPath)
+                        }
+                    }
+                    else -> {
+                        path.scan(""){ acc, p -> "$acc/$p"}.forEach { 
+                            sizes.putIfAbsent(it,0)
+                            sizes[it] = sizes[it]!! + dolOrInt.toInt()
+                        }
+                    }
+                }
+            }
+        return sizes
+    }
+}
+
+fun main(){
+    val d = Day7()
+    println(d.part1())
+    println(d.part2())
 }
--- src/test/kotlin/days/Day7Tests.kt
+++ src/test/kotlin/days/Day7Tests.kt
@@ -1,4 +1,14 @@
 package days
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class Day7Tests {
+    val d= Day7()
+    
+    @Test
+    fun t(){
+        assertEquals(95437, d.part1())
+        assertEquals(24933642, d.part2())
+    }
 }
